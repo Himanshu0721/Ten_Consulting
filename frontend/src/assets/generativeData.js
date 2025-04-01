e47f63f4117a8290adb91ccb2eb67dd5bf694357
yogita0914 <yogitazee1@gmail.com>
2025-04-01 17:08:16 +0530

    change content

--- frontend/src/assets/generativeData.js
+++ frontend/src/assets/generativeData.js
@@ -1,59 +1,57 @@
 export const heroData = {
-    backgroundImage:
-      "url('https://d1ugv6dopk5bx0.cloudfront.net/s3fs-public/2023-05/pawel-czerwinski-PvgqqicSLvA-unsplash_0.webp')",
-    breadcrumb: [
-      { label: "Home", link: "/" },
-      { label: "Services", link: "/our-services" },
-      { label: "AI & ML", link: "/ai-ml" },
-      { label: "AI-Innovation", link: "" }, 
-    ],
-    title: "AI-Innovation",
-    description:
-      "Unleashing the future with AI-transforming ideas into reality, driving creativity, and revolutionizing industries.",
-  };
+  backgroundImage:
+    "url('https://d1ugv6dopk5bx0.cloudfront.net/s3fs-public/2023-05/pawel-czerwinski-PvgqqicSLvA-unsplash_0.webp')",
+  breadcrumb: [
+    { label: "Home", link: "/" },
+    { label: "Services", link: "/our-services" },
+    { label: "AI & ML", link: "/ai-ml" },
+    { label: "AI-Innovation", link: "/ai-ml/generative-ai" },
+  ],
+  title: "AI-Innovation",
+  description:
+    "Unleashing the future with AI-transforming ideas into reality, driving creativity, and revolutionizing industries.",
+};
 
-  export const contentData = {
-    heading:
-      "Unlocking infinite horizons: How AI-Innovation empowers industries with cutting-edge creativity, innovation, and inspiration.",
-    paragraphs: [
-      "Generative AI is revolutionizing industries with cutting-edge creativity, innovation, and inspiration.",
-      "Welcome to the transformative world of AI! At Ten Consulting, we believe that Generative AI holds the key to revolutionizing industries and reshaping the way we interact with technology. Our innovative Generative AI services harness the power of cutting-edge AI and natural language processing (NLP) to create intelligent systems that can comprehend and respond to human language in ways previously unimaginable.",
-      "Content creation and marketing: With our advanced Generative AI solutions, businesses can automate content generation, from blog posts and articles to social media updates. This not only saves time but also ensures consistent, high-quality content that resonates with target audiences, leading to improved engagement and brand loyalty.",
-      "Leveraging Generative AI, Ten Consulting helps businesses streamline customer interactions with intelligent virtual assistants. These AI-powered assistants offer 24/7 support, answering customer queries, providing personalized recommendations, and enhancing overall customer experience, resulting in increased customer satisfaction and retention.",
-      "Our Generative AI offerings are built on state-of-the-art AI frameworks and tools, including sophisticated NLP models, LLMs, neural networks, and deep learning algorithms. These cutting-edge technologies empower businesses to automate repetitive tasks, gain valuable customer insights, and optimize decision-making processes, transforming their operations and fostering innovation at scale.",
-      "At Ten Consulting, we boast a remarkable capability to fine-tune foundation models, harnessing their latent power to derive impactful changes. Our expertise in Generative AI enables us to explore endless possibilities for your business's growth and success, as we explore new frontiers in AI-driven transformation.",
-    ],
-  };
-  
-  export const offerings = [
-    {
-      title: "Chatbots and Virtual Assistants",
-      content:
-        "Our Chatbots/Virtual Assistants services encompass AI-powered chatbot development, virtual assistant integration, and natural language processing (NLP) capabilities.",
-    },
-    {
-      title: "Skill Assessment Services",
-      content:
-        "Kellton's skill assessment offering incorporates a powerful tool capable of generating dynamic questions and accurately assessing candidate responses. Our rating system ensures a transparent evaluation of the candidate's skill level, providing valuable insights for informed decision-making.",
-    },
-    {
-      title: "Credit Analysis",
-      content:
-        "Our Credit Analysis services encompass advanced data analysis and Generative AI algorithms that can generate synthetic data to augment the limited labeled data for credit risk modeling.",
-    },
-    {
-      title: "Knowledge Databases",
-      content:
-        "Leveraging Generative AI, we enable businesses to automatically generate high-quality and relevant content, create concise summaries of large documents, and structure knowledge for efficient retrieval and analysis.",
-    },
-    {
-      title: "AI Generated Product/Marketing Campaign",
-      content:
-        "Our AI-generated product/marketing campaign services encompass AI-powered content creation, product design, and marketing campaign optimization.",
-    },
-  ];
+export const contentData = {
+  heading:
+    "Unlocking infinite horizons: How AI-Innovation empowers industries with cutting-edge creativity, innovation, and inspiration.",
+  paragraphs: [
+    "Generative AI is revolutionizing industries with cutting-edge creativity, innovation, and inspiration.",
+    "Welcome to the transformative world of AI! At Ten Consulting, we believe that Generative AI holds the key to revolutionizing industries and reshaping the way we interact with technology. Our innovative Generative AI services harness the power of cutting-edge AI and natural language processing (NLP) to create intelligent systems that can comprehend and respond to human language in ways previously unimaginable.",
+    "Content creation and marketing: With our advanced Generative AI solutions, businesses can automate content generation, from blog posts and articles to social media updates. This not only saves time but also ensures consistent, high-quality content that resonates with target audiences, leading to improved engagement and brand loyalty.",
+    "Leveraging Generative AI, Ten Consulting helps businesses streamline customer interactions with intelligent virtual assistants. These AI-powered assistants offer 24/7 support, answering customer queries, providing personalized recommendations, and enhancing overall customer experience, resulting in increased customer satisfaction and retention.",
+    "Our Generative AI offerings are built on state-of-the-art AI frameworks and tools, including sophisticated NLP models, LLMs, neural networks, and deep learning algorithms. These cutting-edge technologies empower businesses to automate repetitive tasks, gain valuable customer insights, and optimize decision-making processes, transforming their operations and fostering innovation at scale.",
+    "At Ten Consulting, we boast a remarkable capability to fine-tune foundation models, harnessing their latent power to derive impactful changes. Our expertise in Generative AI enables us to explore endless possibilities for your business's growth and success, as we explore new frontiers in AI-driven transformation.",
+  ],
+};
 
-  
+export const offerings = [
+  {
+    title: "Chatbots and Virtual Assistants",
+    content:
+      "Our Chatbots/Virtual Assistants services encompass AI-powered chatbot development, virtual assistant integration, and natural language processing (NLP) capabilities.",
+  },
+  {
+    title: "Skill Assessment Services",
+    content:
+      "Kellton's skill assessment offering incorporates a powerful tool capable of generating dynamic questions and accurately assessing candidate responses. Our rating system ensures a transparent evaluation of the candidate's skill level, providing valuable insights for informed decision-making.",
+  },
+  {
+    title: "Credit Analysis",
+    content:
+      "Our Credit Analysis services encompass advanced data analysis and Generative AI algorithms that can generate synthetic data to augment the limited labeled data for credit risk modeling.",
+  },
+  {
+    title: "Knowledge Databases",
+    content:
+      "Leveraging Generative AI, we enable businesses to automatically generate high-quality and relevant content, create concise summaries of large documents, and structure knowledge for efficient retrieval and analysis.",
+  },
+  {
+    title: "AI Generated Product/Marketing Campaign",
+    content:
+      "Our AI-generated product/marketing campaign services encompass AI-powered content creation, product design, and marketing campaign optimization.",
+  },
+];
 
 export const justSectionDetails = {
   backgroundImage: "url('https://cdn.kellton.com/s3fs-public/large-bg.webp')",
@@ -87,11 +85,14 @@ export const justOfferings = [
 ];
 
 export const funFactDetails = {
-  imageSrc: "https://d1ugv6dopk5bx0.cloudfront.net/s3fs-public/Purple%20(1).webp",
-  iconSrc: "https://d3tj4wnms16o5e.cloudfront.net/s3fs-public/fun-fact-star.png",
+  imageSrc:
+    "https://d1ugv6dopk5bx0.cloudfront.net/s3fs-public/Purple%20(1).webp",
+  iconSrc:
+    "https://d3tj4wnms16o5e.cloudfront.net/s3fs-public/fun-fact-star.png",
   title: "AI FACT",
   fact: "AI-powered chatbots handle 85% of customer interactions, improving efficiency and satisfaction.",
-  description: "Embrace AI-driven customer service and elevate engagement like never before.",
+  description:
+    "Embrace AI-driven customer service and elevate engagement like never before.",
 };
 
 export const transformationData = [
@@ -99,7 +100,8 @@ export const transformationData = [
     img: "https://d1ugv6dopk5bx0.cloudfront.net/s3fs-public/styles/case_studies_550px_460px/public/2023-07/omnichannel-excellence-hospitality-leader%20%28Thumbnail%29.webp?itok=060k_l3C",
     alt: "AI in Hospitality",
     region: "APAC",
-    title: "Elevating customer experiences with AI-powered hospitality solutions",
+    title:
+      "Elevating customer experiences with AI-powered hospitality solutions",
   },
   {
     img: "https://d1ugv6dopk5bx0.cloudfront.net/s3fs-public/styles/case_studies_550px_460px/public/2023-08/Empowering%20the%20legal%20landscape%20Thumbnail.webp?itok=GantJ2pv",
@@ -134,10 +136,8 @@ export const insightsData = [
   },
   {
     img: "https://d1ugv6dopk5bx0.cloudfront.net/s3fs-public/styles/case_studies_550px_460px/public/AI%20for%20Data%20Analysis.webp?itok=thNRESzz",
-    title: "Blockchain Beyond Cryptocurrency: The Future of Secure Transactions",
+    title:
+      "Blockchain Beyond Cryptocurrency: The Future of Secure Transactions",
     date: "July 20, 2024",
   },
 ];
-
-
-
