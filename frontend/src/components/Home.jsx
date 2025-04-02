b3ce5bb29ab37df3199c5085274c345647904c49
Riya Singh <riyasingh606065@gmail.com>
2025-04-02 19:16:09 +0530

    navbar and footer

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,19 @@
 import {React, useState } from "react";
-
+import homeImage from "../assets/bronze banner.webp";
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <div className="w-full h-screen">
+    <img src={homeImage} alt=" home" className="w-full h-full object-cover" />
+      {/* <h1 className="text-3xl font-bold text-blue-600">
+        Hello, It is Ten Consulting
+      </h1>
+      <p className="text-lg text-gray-700 mt-2">
+        Welcome to our platform. We provide innovative solutions to grow your
+        business.
+      </p>
+      <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+        Learn More
+      </button>
       <h1 className="text-3xl font-bold text-blue-600">
         Hello, It is Ten Consulting
       </h1>
@@ -13,6 +24,46 @@ function Home() {
       <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
         Learn More
       </button>
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello, It is Ten Consulting
+      </h1>
+      <p className="text-lg text-gray-700 mt-2">
+        Welcome to our platform. We provide innovative solutions to grow your
+        business.
+      </p>
+      <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+        Learn More
+      </button>
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello, It is Ten Consulting
+      </h1>
+      <p className="text-lg text-gray-700 mt-2">
+        Welcome to our platform. We provide innovative solutions to grow your
+        business.
+      </p>
+      <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+        Learn More
+      </button>
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello, It is Ten Consulting
+      </h1>
+      <p className="text-lg text-gray-700 mt-2">
+        Welcome to our platform. We provide innovative solutions to grow your
+        business.
+      </p>
+      <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+        Learn More
+      </button>
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello, It is Ten Consulting
+      </h1>
+      <p className="text-lg text-gray-700 mt-2">
+        Welcome to our platform. We provide innovative solutions to grow your
+        business.
+      </p>
+      <button className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+        Learn More
+      </button> */}
     </div>
   );
 }
