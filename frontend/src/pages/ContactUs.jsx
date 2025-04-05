5335ffa19735c60a141f84f102b31dc8629dc802
Ashish Agarwal <code.with.aasheesh@gmail.com>
2025-04-05 22:02:41 +0530

    contact page done

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -3,20 +3,13 @@ import {
   heroData,
   contentData,
   funFactDetails,
-  //   offerings,
-  //   justSectionDetails,
-  //   insightsData,
-  //   transformationData,
 } from "../assets/contactData";
 
 import HeroSection from "../components/HeroSection";
 import ContentSection from "../components/ContentSection";
-// import OfferingsSection from "../../../components/OfferingsSection";
-// import JustSection from "../../../components/JustFewSection";
 import FunFactSection from "../components/FunFactSection";
 import ContactForm from "../components/ContactForm";
-// import TransformationSection from "../../../components/LeadingSection";
-// import InsightSection from "../../../components/InsightSection";
+import ContactInfo from "../components/ContactInfo";
 
 const ContactUs = () => {
   return (
@@ -27,17 +20,7 @@ const ContactUs = () => {
       <FunFactSection funFactDetails={funFactDetails} />
 
       <ContactForm />
-      {/* 
-
-      <OfferingsSection offerings={offerings} />
-
-      <JustSection justSectionDetails={justSectionDetails} />
-
-      <FunFactSection funFactDetails={funFactDetails} />
-
-      <TransformationSection transformationData={transformationData} />
-
-      <InsightSection insightsData={insightsData} /> */}
+      <ContactInfo />
     </div>
   );
 };
